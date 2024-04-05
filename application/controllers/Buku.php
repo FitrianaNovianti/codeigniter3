873b46446500963609b5296612f5f3570bff4256
FitrianaNovianti <fitriana1120@gmail.com>
2024-04-05 21:43:52 +0700

    first commit

---
+++ application/controllers/Buku.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script acces allowed');
+
+class Buku extends CI_Controller {
+    public function index()
+    {
+        $this->load->view('Buku/tampil_buku');
+    }
+
+    function create()
+    {
+        $this->load->view('Buku/tambah_buku');
+    }
+}
